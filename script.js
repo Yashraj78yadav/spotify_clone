8d04393f53bce177926bdbe6903edbf060f9e466
Yashraj78yadav <richyashrajyadav@gmail.com>
2024-03-13 12:33:59 +0530

    spotify clone

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ console.log("welcome to my spotify")
 let songIndex =0;
 let audioElement=new Audio('1song.mp3');
 let masterplay=document.getElementById('masterplay');
-let progressBar=document.getElementById('progressBar');
+let progressBar=document.getElementById('progress_bar');
+let gif=document.getElementById('gif');
 let songs=[
     {sonngName:"pawan-star",filepath:"file/1.mp3",coverpath:"cover/1.jpg"},
     {sonngName:"pawan-star",filepath:"file/2.mp3",coverpath:"cover/1.jpg"},
@@ -15,14 +16,19 @@ masterplay.addEventListener('click',()=>{
         audioElement.play();
         masterplay.classList.remove('fa-play-circle');
         masterplay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
     }
     else{
         audioElement.pause();
         masterplay.classList.remove('fa-pause-circle');
         masterplay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
     }
 })
 
-progressBar.addEventListener('timeupdate',()=>{
-    console.log('timeupdate');
+audioElement.addEventListener('timeupdate',()=>{
+    // console.log('timeupdate');
+    progress = parseInt((audioElement.currentTime/audioElement.duration)*100);
+    //  console.log(progress);
+    progress_bar.value=progress;
 })
